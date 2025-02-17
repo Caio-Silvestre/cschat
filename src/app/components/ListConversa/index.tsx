c1892ad69d131ee18849393bedf61b27ec9a7dca
Caio S.G <caiosilvestre.dev@gmail.com>
2025-02-17 18:20:49 -0300

    adjust imports

--- src/app/components/ListConversa/index.tsx
+++ src/app/components/ListConversa/index.tsx
@@ -1,16 +1,17 @@
 "use client";
 import React, { Fragment, useEffect, useState } from "react";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import Divider from "@mui/material/Divider";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import Avatar from "@mui/material/Avatar";
-import Typography from "@mui/material/Typography";
+import {
+  List,
+  ListItemButton,
+  ListItemAvatar,
+  ListItemText,
+  Avatar,
+  Divider,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 import { dataFormatada } from "@/app/utils";
 import { Conversa } from "./interfaces";
-import { ListItemButton } from "@mui/material";
 
 const ListConversa: React.FC = () => {
   const [dadosConversa, setDadosConversa] = useState<Conversa[]>([]);
